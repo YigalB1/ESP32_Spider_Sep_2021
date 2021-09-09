4a9c85e8464e939f22995f8511ee5a50642621d8
YigalB1 <yigalb@hotmail.com>
2021-09-09 20:34:29 +0300

    start coding

--- Spider_Control_ESP32/src/main.cpp
+++ Spider_Control_ESP32/src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
+const int red_led_pin = 23;
+const int Yellow_led_pin = 22;
+const int green_led_pin = 21;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(red_led_pin, OUTPUT);
+  pinMode(Yellow_led_pin, OUTPUT);
+  pinMode(green_led_pin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(red_led_pin, HIGH);  
+  digitalWrite(Yellow_led_pin, HIGH);  
+  digitalWrite(green_led_pin, HIGH);  
+  delay(1000);                  // wait for a second
+   digitalWrite(red_led_pin, LOW);  
+  digitalWrite(Yellow_led_pin, LOW);  
+  digitalWrite(green_led_pin, LOW);  
+  delay(1000);
 }
